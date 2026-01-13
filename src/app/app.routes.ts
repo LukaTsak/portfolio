967adcbd5a59717d9cec44869d5b9538b24613cd
LukaTsak <lukawaqadze01@gmail.com>
2026-01-13 14:30:36 +0400

    portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Desktop } from './desktop/desktop';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo : 'desktop', pathMatch: 'full' },
+    {path: 'desktop', component: Desktop, pathMatch: 'full' },
+];
---
+++ src/app/desktop/desktop.ts
@@ -0,0 +1,12 @@
+import { CdkDrag } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop',
+  imports: [CdkDrag],
+  templateUrl: './desktop.html',
+  styleUrl: './desktop.scss',
+})
+export class Desktop {
+
+}

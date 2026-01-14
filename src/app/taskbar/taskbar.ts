fe60ac61fc7ea0a1d69ce11ebb92af7bf1173080
LukaTsak <lukatsakadze10@gmail.com>
2026-01-14 10:51:49 +0400

    visually complete taskbar and desktop

--- src/app/taskbar/taskbar.ts
+++ src/app/taskbar/taskbar.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-taskbar',
-  imports: [],
   templateUrl: './taskbar.html',
+  imports: [DatePipe],
   styleUrl: './taskbar.scss',
 })
-export class Taskbar {
+export class Taskbar implements OnInit, OnDestroy {
+  currentDate = new Date();
+  private timerId!: number;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.timerId = window.setInterval(() => {
+      this.currentDate = new Date();
+      this.cdr.markForCheck();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timerId);
+  }
 }
